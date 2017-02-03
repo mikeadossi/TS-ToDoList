ed84f8817c467318392d3ae9227a4cbd0bf097ec
starrpatty28 <starrpatty28@gmail.com>
2017-02-03 09:24:26 -0800

    create nav.js component, got it to render on localhost:8080 and import from Input

---
+++ src/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Input from 'input';
+
+export default class Nav extends React.Component{
+
+render(){
+  return(
+    <div>
+      <h1>Hot Tamales</h1>
+      <Input />
+    </div>
+  )
+}
+}
