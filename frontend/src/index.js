04c547851648e1e91396ebab4e0bc6d2fe034dcf
starrpatty28 <starrpatty28@gmail.com>
2017-02-02 17:06:02 -0800

    get webpack running

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-render(
-  document.getElementById('root')
-)
-
-//This represent the react DOM
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from 'app.js'
+
+render(
+  <App />,
+  document.getElementById('root')
+)
+
+//This represent the react DOM
