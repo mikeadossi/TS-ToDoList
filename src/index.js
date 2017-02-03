21ca12113864f93e9733082e688c7e0128184db6
starrpatty28 <starrpatty28@gmail.com>
2017-02-03 09:17:18 -0800

    deleted src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from 'app.js'
+import Nav from 'nav.js'
 
 render(
-  <App />,
+  <Nav />,
+    
   document.getElementById('root')
 )
 
